5cdd1080e578448d75bb00be874513f3eafe0116
gxiaosong <lyricsong47@gmail.com>
2025-09-25 09:42:04 +0000

    feat: remove product stream

---
+++ Framework/Api/Acl/Admin/Role/AclRoleCollection.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types=1);
+
+namespace HeyFrame\Core\Framework\Api\Acl\Admin\Role;
+
+use HeyFrame\Core\Framework\DataAbstractionLayer\EntityCollection;
+use HeyFrame\Core\Framework\Log\Package;
+
+/**
+ * @extends EntityCollection<AclRoleEntity>
+ */
+#[Package('framework')]
+class AclRoleCollection extends EntityCollection
+{
+    public function getApiAlias(): string
+    {
+        return 'dal_acl_role_collection';
+    }
+
+    protected function getExpectedClass(): string
+    {
+        return AclRoleEntity::class;
+    }
+}
